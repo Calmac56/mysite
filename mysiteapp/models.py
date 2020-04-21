a352791f19b90268f0d29cf33482d1b7916e34eb
Calmac56 <60009981+Calmac56@users.noreply.github.com>
2020-04-21 01:29:08 +0100

    Started ajax

--- mysiteapp/models.py
+++ mysiteapp/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class projects(models.Model):
+
+class Language(models.Model):
+    name = models.CharField(primary_key= True, max_length=200)
+
+
+
+class Projects(models.Model):
     name = models.CharField(max_length=200)
     datecreated = models.DateField()
     lastmodified = models.DateField()
     views = models.IntegerField(default=0)
+    languages = models.ForeignKey(Language, on_delete=models.CASCADE, blank=True, null= True)
